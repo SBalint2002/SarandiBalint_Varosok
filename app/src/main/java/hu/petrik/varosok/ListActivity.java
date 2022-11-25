72739c423f4a98f40371c9ffac83276cd26681ea
Sárándi <sarandi.balint@gmail.com>
2022-11-25 12:04:12 +0100

    adatok kilistázása

--- app/src/main/java/hu/petrik/varosok/ListActivity.java
+++ app/src/main/java/hu/petrik/varosok/ListActivity.java
@@ -27,7 +27,7 @@ public class ListActivity extends AppCompatActivity {
 
     private TextView listView;
     private Button visszaButton;
-    private static final String baseUrl = "https://retoolapi.dev/TLuyCO/varosok";
+    private static final String baseUrl = "https://retoolapi.dev/tBJYiU/varosok";
     private List<City> cities = new ArrayList<>();
 
     @Override
@@ -48,5 +48,88 @@ public class ListActivity extends AppCompatActivity {
     private void init() {
         listView = findViewById(R.id.listView);
         visszaButton = findViewById(R.id.visszaButton);
+        listView.setMovementMethod(new ScrollingMovementMethod());
+        RequestTask task= new RequestTask("GET");
+        task.execute();
+    }
+
+    private class RequestTask extends AsyncTask<Void, Void, Response>{
+        private String requestUrl;
+        private String requestMethod;
+        private String requestBody;
+
+        public RequestTask(String requestUrl) {
+            this.requestUrl = requestUrl;
+            this.requestMethod = "GET";
+        }
+
+        public RequestTask(String requestUrl, String requestMethod) {
+            this.requestUrl = requestUrl;
+            this.requestMethod = requestMethod;
+        }
+
+        public RequestTask(String requestUrl, String requestMethod, String requestBody) {
+            this.requestUrl = requestUrl;
+            this.requestMethod = requestMethod;
+            this.requestBody = requestBody;
+        }
+
+        @Override
+        protected Response doInBackground(Void... voids) {
+            Response response = null;
+            try {
+                switch (requestMethod){
+                    case "GET":
+                        response = RequestHandler.get(baseUrl);
+                        break;
+                    case "POST":
+                        response = RequestHandler.post(requestUrl, requestBody);
+                        break;
+                    case "PUT":
+                        response = RequestHandler.put(requestUrl, requestBody);
+                        break;
+                    case "DELETE":
+                        response = RequestHandler.delete(requestUrl);
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Response response){
+            Gson converter = new Gson();
+            super.onPostExecute(response);
+            if(response == null){
+                Toast.makeText(ListActivity.this, "unable_to_connect", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(response.getResponseCode() >= 400){
+                Toast.makeText(ListActivity.this, response.getContent(), Toast.LENGTH_SHORT).show();
+                return;
+            }
+            switch (requestMethod){
+                case "GET":
+                    String content = response.getContent();
+                    cities = Arrays.asList(converter.fromJson(content, City[].class));
+                    System.out.println("!"+cities);
+                    listView.setText(cities.toString());
+                    break;
+                default:
+                    if (response.getResponseCode() >= 201 && response.getResponseCode() < 300){
+                        listView.setText("");
+                        RequestTask task = new RequestTask(baseUrl);
+                        task.execute();
+                    }
+                    break;
+            }
+        }
     }
 }
